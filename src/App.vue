5815a86775e50e13510106990e66f64717e74f93
golinka <artem.holinka@gmail.com>
2019-03-23 17:51:11 +0200

    feat(layout): added details section

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,47 @@
 <template>
   <main id="app">
+    <section class="details section-container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="">Contacts</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-12">
+          <h2 class="details__subtitle">
+            <span>GMS</span> Headquarters
+          </h2>
+          <div class="row">
+            <div class="col-sm-6 col-12">
+              <p class="mb-5">Global Message Services Ukraine LLC
+                Bundesstrasse 5 | 3rd Floor
+                6300 Zug
+                Switzerland
+              </p>
+              <a href="tel:+41415446200">+41 41 544 62 00</a>
+            </div>
+            <div class="details__contacts col-sm-6 col-12">
+              <p>Email</p>
+              <a href="mailto:info@gms-worldwide.com">info@gms-worldwide.com</a>
+              <p>Send message</p>
+              <a href="viber://add?number=%2B41415446200">VIBER US</a>
+              <p>Email</p>
+              <a href="mailto:info@gms-worldwide.com">info@gms-worldwide.com</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-12">
+          <h2 class="details__subtitle">
+            Technical support <span>24/7</span>
+          </h2>
+          <div class="row">
+            <div class="col-12">
+              <p class="mb-5">GMS understands that it can be hard to follow all new technologies. We regularly provide for all our clients educational trainings to make your operator`s business earn more and better though our best practices.</p>
+              <a href="tel:+41415446206">+41 41 544 62 06</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
